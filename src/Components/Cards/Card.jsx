53ae46d8f1ec15fd968e05134fc73de9857dc0ea
thoddusamy <thoddusamyg@gmail.com>
2022-07-27 15:34:07 +0530

    edited

--- src/Components/Cards/Card.jsx
+++ src/Components/Cards/Card.jsx
@@ -8,17 +8,19 @@ const Card = ({ filterCountries }) => {
             {
                 filterCountries.map((country, index) => {
                     return (
-                        <Link to={`/country/${country.cca3}`} className="col card_column" key={index}>
-                            <div className="card__container">
-                                <img src={country.flags.svg} alt={country.name.common} />
-                                <div className="desc">
-                                    <h5>Country: {country.name.common}</h5>
-                                    <h5>Region: {country.region}</h5>
-                                    <h5>Popu: {country.population}</h5>
-                                    <h5>Area(Km<sup>2</sup>): {country.area}</h5>
+                        <>
+                            <Link to={`/country/${country.cca3}`} className="col card_column" key={index}>
+                                <div className="card__container">
+                                    <img src={country.flags.svg} alt={country.name.common} />
+                                    <div className="desc">
+                                        <h5>Country: {country.name.common}</h5>
+                                        <h5>Region: {country.region}</h5>
+                                        <h5>Popu: {country.population}</h5>
+                                        <h5>Area: {country.area}</h5>
+                                    </div>
                                 </div>
-                            </div>
-                        </Link>
+                            </Link>
+                        </>
                     )
                 })
             }
